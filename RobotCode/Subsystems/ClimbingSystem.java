bc7e27f007bc8c3a54c00375c226a2af5723a26e
FRC1555 <59070708+FRC1555@users.noreply.github.com>
2022-03-04 13:49:18 -0500

    Update ClimbingSystem.java

--- RobotCode/Subsystems/ClimbingSystem.java
+++ RobotCode/Subsystems/ClimbingSystem.java
@@ -6,24 +6,53 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import frc.robot.Constants;
 
 public class ClimbingSystem extends SubsystemBase {
+  //Defining all systems
+  public PneumaticsControlModule ThePCM;
+  public Compressor PCMCompressor;
+  public Compressor PHCompressor;
   public DoubleSolenoid IntakeSolenoid;
   public DoubleSolenoid ClimbingSolenoid;
-  public static VictorSPX ClimberWinch;
-  
+  public VictorSPX ClimberWinch;
+  public Encoder WinchEncoder;
+
   /** Creates a new ClimbingSystem. */
   public ClimbingSystem() {
-    IntakeSolenoid = new DoubleSolenoid(0, PneumaticsModuleType.CTREPCM , Constants.IntakePneumatic_ID_A, Constants.IntakePneumatic_ID_B);
-    ClimbingSolenoid = new DoubleSolenoid(1, PneumaticsModuleType.CTREPCM, Constants.ClimbingPneumatic_ID_A, Constants.ClimbingPneumatic_ID_B);
-    ClimberWinch = new VictorSPX(Constants.ClimberWinch_ID);
+    /*Instantiating Pneumatics Control Module (Controls Solenoids) 
+    and Pneumatics Hub (Controls Compressor)*/
+    PCMCompressor = new Compressor(9, PneumaticsModuleType.CTREPCM);
+    PHCompressor = new Compressor(10, PneumaticsModuleType.REVPH);
+
+    //PCMCompressor.enableDigital();
+    //PHCompressor.disable();
+
+    // boolean enabled = PCMCompressor.enabled();
+    // boolean pressureSwitch = PCMCompressor.getPressureSwitchValue();
+    // double current = PHCompressor.getCurrent();
+
+    DoubleSolenoid IntakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    DoubleSolenoid ClimbingSolenoid= new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+
+    ClimberWinch = new VictorSPX(Constants.climberWinch_ID);
+
+    WinchEncoder = new Encoder(Constants.WinchEncoder_ID_A, Constants.WinchEncoder_ID_B);
+
+
+    ClimberWinch.setInverted(true);
   }
   
+  public void ClimberWinch(double speed){
+    ClimberWinch.set(ControlMode.PercentOutput, speed);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
